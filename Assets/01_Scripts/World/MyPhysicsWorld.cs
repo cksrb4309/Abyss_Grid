6684b8e390bfc6f4b862d4023622bb1c23f915ae
cksrb4309 <rlacksrb0856@gmail.com>
2026-03-17 02:54:37 +0900

    feat: implement basic PhysicsWorld logic
    기본적인 PhysicsWorld를 위한 기초 구현
     - 씬마다 MyPhysicsWorld 컴포넌트를 붙인 게임오브젝트가 존재해야함

--- Assets/01_Scripts/World/MyPhysicsWorld.cs
+++ Assets/01_Scripts/World/MyPhysicsWorld.cs
@@ -1,16 +1,72 @@
 using UnityEngine;
+using UnityEngine.LowLevelPhysics2D;
 
+/// <summary>
+/// PhysicsWorld 사용법
+/// 
+/// Body 생성
+/// CreateBody(PhysicsBodyDefinition def) : PhysicsBody
+/// MyPhysicsWorld.Instance.World.CreateBody(def)
+/// 
+/// Shape 생성
+/// CreateShape(Geometry geometry, PhysicsShapeDefinition shapeDef) : void
+/// PhysicsBody body = _world.CreateBody(bodyDef);
+/// body.CreateShape(geometry, shapeDef);
+/// 
+/// 
+/// Box Shape 생성
+/// groundBody.CreateShape(
+///     PolygonGeometry.CreateBox(
+///         size: new Vector2(x: width, y: height),
+///         radius: 0f,
+///         transform: new PhysicsTransform(
+///             position: new Vector2(x: x좌표, y: y좌표),
+///             rotation: PhysicsRotate.identity
+///         ),
+///     PhysicsShapeDefinition.defaultDefinition)
+/// );
+/// 
+/// 
+/// 
+/// </summary>
 public class MyPhysicsWorld : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public static MyPhysicsWorld Instance { get; private set; }
+
+    private PhysicsWorld _world;
+    public PhysicsWorld World => _world;
+
+    //[SerializeField] private PhysicsLowLevelSettings2D settings;
+    [SerializeField] private bool simulateInFixedUpdate = true;
+
+    [SerializeField] private PhysicsShapeDefinition shapeDef;
+    [SerializeField] private PhysicsBodyDefinition bodyDef;
+
+    private void Awake()
     {
-        
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+
+        CreateWorld();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        if (!simulateInFixedUpdate) return;
+
+        Simulate(Time.fixedDeltaTime);
+    }
+    private void CreateWorld()
+    {
+        _world = PhysicsWorld.defaultWorld;
+    }
+    public void Simulate(float deltaTime)
+    {
+        _world.Simulate(deltaTime);
     }
 }
